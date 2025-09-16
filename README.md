9986b8a17c41de5b034e4378a66e43260d65a0e2
David Faden (revfad.com) <dfaden@gmail.com>
2025-09-15 23:27:16 -0700

    Factor out JS
    Also set focus to the main entry field by default.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Flip text
 
 Uses Unicode to make text look like it's been rotated 180 degrees.
+
+## Acknowledgments
+
+I got the idea to do this from pne. (See http://pne.livejournal.com/398399.html,
+but don't comment on it! In retrospect, it may have been a mistake to link to that
+since a lot of people bugged him with complaints that should have gone to me. Sorry.)
+
+Thank you to Brook Monroe, Tim McCormack, Glards, and Yeeliberto amongst
+others for further suggestions.
+
+Formatted with https://prettier.io/.
---
+++ flip.js
@@ -0,0 +1,58 @@
+const FLIP_TABLE = {
+  a: "\u0250",
+  b: "q",
+  c: "\u0254", // open o -- from pne
+  d: "p",
+  e: "\u01DD",
+  f: "\u025F", // from pne
+  g: "\u0183",
+  h: "\u0265",
+  i: "\u0131", // from pne
+  j: "\u027E",
+  k: "\u029E",
+  //l : '\u0283',
+  m: "\u026F",
+  n: "u",
+  r: "\u0279",
+  t: "\u0287",
+  v: "\u028C",
+  w: "\u028D",
+  y: "\u028E",
+  ".": "\u02D9",
+  "[": "]",
+  "(": ")",
+  "{": "}",
+  "?": "\u00BF", // from pne
+  "!": "\u00A1",
+  "\'": ",",
+  "<": ">",
+  _: "\u203E",
+  "\u203F": "\u2040",
+  "\u2045": "\u2046",
+  "\u2234": "\u2235",
+  "\r": "\n", // thank you, Yeeliberto
+};
+
+for (let c in FLIP_TABLE) {
+  FLIP_TABLE[FLIP_TABLE[c]] = c;
+}
+
+function flipString(aString) {
+  const last = aString.length - 1;
+  const result = [];
+  for (let i = last; i >= 0; --i) {
+    let c = aString[i];
+    let r = FLIP_TABLE[c];
+    result[last - i] = r ? r : c;
+  }
+  return result.join("");
+}
+
+const originalArea = document.getElementById("original");
+const flippedArea = document.getElementById("flipped");
+originalArea.addEventListener("keyup", () => {
+  flippedArea.value = flipString(originalArea.value.toLowerCase());
+});
+
+originalArea.focus();
+originalArea.select();
